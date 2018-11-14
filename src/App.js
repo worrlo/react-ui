d28169321280cfa030ff3332cee37e2873de7663
John Worrall <theworrall@gmail.com>
2018-11-13 23:37:05 -0500

    implements basic app router using dispatch

--- src/App.js
+++ src/App.js
@@ -1,40 +1,67 @@
 import React, { useEffect, useReducer } from 'react';
 
-const appName = "React(HK)"
+const app = {
+  abr: "React(HK)",
+  name: "React-Hook UI Demo"
+};
+
 function App(props) {
   const [state, dispatch] = useReducer((state, action) => {
+    let res = {...state, lastAction: action.type};
     switch(action.type) {
-      case 'LOGOUT': return {
-        ...state,
-        isLoggedIn: false,
-        abr: appName
-      }
-      case 'LOGIN': return {
-        ...state,
-        isLoggedIn: true,
-        abr: (appName + " Admin")
-      }
-      default: return state;
+      case '#home': 
+      case '#signOut': 
+          res.isLoggedIn = false;
+          res.abr = app.abr;
+        break;
+      case '#login': 
+          res.isLoggedIn = true;
+          res.abr = (app.abr + " Admin");
+        break;
+      case '#docs':
+        break;
+      default: 
+        res.lastAction = "";
+      break;
     }
+    if (res.lastAction && action.event) {
+      action.event.preventDefault();
+      console.log("nav-event: ", res.lastAction);
+    }
+    return res;
   },{
     isLoggedIn: false,
-    abr: appName,
-    name: "React-Hook UI Demo"
+    lastAction: "",
+    ...app
   });
 
+  function handler(e) {
+    if (e.target.nodeName === "A") {
+      dispatch({type: e.target.hash, event: e});
+    }
+  } 
+
+  //Render Icon(s)
   useEffect(() => {
     window.feather.replace();
   });
 
+  useEffect(() => {
+    document.addEventListener('click', handler);
+    return () => {
+      document.removeEventListener('click', handler);
+    };
+  });
+
   return (
     <React.Fragment>
-      <AppNav dispatch={dispatch} {...state}>
-        {state.isLoggedIn ? <AppSignOut dispatch={dispatch} /> : ""}
+      <AppNav {...state}>
+        {state.isLoggedIn ? <AppSignOut /> : ""}
       </AppNav>
       <main role="main">
         {state.isLoggedIn
           ? "<AppContent />"
-          : <WelcomePage dispatch={dispatch}  {...state} />
+          : <WelcomePage  {...state} />
         }
       </main>
     </React.Fragment>
@@ -44,9 +71,8 @@ function App(props) {
 function AppNav(props) {
   return (
     <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-      <a href="#home" 
+      <a href="http://www.google.com#home" 
         className="navbar-brand col-sm-3 col-md-2 mr-0"
-        onClick={() => props.dispatch({type: props.isLoggedIn ? "LOGOUT" : "LOGIN"})}
         >{props.abr}</a>
       {props.children}
     </nav>
@@ -57,7 +83,7 @@ function AppSignOut(props) {
   return (
     <ul className="navbar-nav px-3">
       <li className="nav-item text-nowrap">
-        <a href="#signOut" className="nav-link" onClick={() => props.dispatch({type:"LOGOUT"})}>
+        <a href="#signOut" className="nav-link">
             Sign Out <span data-feather="log-out"></span>
         </a>
       </li>
@@ -101,7 +127,7 @@ function WelcomePage(props) {
             Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. 
           </p>
           <p>
-            <a href="#login" role="button" className="btn btn-secondary" onClick={() => props.dispatch({type:"LOGIN"})}>Launch</a>
+            <a href="#login" role="button" className="btn btn-secondary" >Launch</a>
           </p>
         </div>
       </div>
