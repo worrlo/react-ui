852159a90e2263a462ac73e6ce322f78df77c65f
John Worrall <theworrall@gmail.com>
2018-11-13 21:52:34 -0500

    welcome-page updated

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React, { useEffect, useReducer } from 'react';
 
-const appName = "React(HK) UI"
+const appName = "React(HK)"
 function App(props) {
   const [state, dispatch] = useReducer((state, action) => {
     switch(action.type) {
       case 'LOGOUT': return {
         ...state,
-        isLoggedIn: false, 
-        name: appName
+        isLoggedIn: false,
+        abr: appName
       }
       case 'LOGIN': return {
         ...state,
         isLoggedIn: true,
-        name: (appName + " Admin")
+        abr: (appName + " Admin")
       }
       default: return state;
     }
-  },{isLoggedIn:false, name: appName});
+  },{
+    isLoggedIn: false,
+    abr: appName,
+    name: "React-Hook UI Demo"
+  });
 
   useEffect(() => {
     window.feather.replace();
@@ -30,7 +34,7 @@ function App(props) {
       <main role="main">
         {state.isLoggedIn
           ? "<AppContent />"
-          : <WelcomePage name={state.name} dispatch={dispatch} />
+          : <WelcomePage dispatch={dispatch}  {...state} />
         }
       </main>
     </React.Fragment>
@@ -43,7 +47,7 @@ function AppNav(props) {
       <a href="#home" 
         className="navbar-brand col-sm-3 col-md-2 mr-0"
         onClick={() => props.dispatch({type: props.isLoggedIn ? "LOGOUT" : "LOGIN"})}
-        >{props.name}</a>
+        >{props.abr}</a>
       {props.children}
     </nav>
   );
@@ -76,8 +80,10 @@ function WelcomePage(props) {
   return (
     <div className="container">
       <div className="jumbotron">
-        <h1 className="display-3">{props.name}</h1>
-        <h3 className="display-4">Welcome</h3>
+        <h1 className="display-3">
+          {props.name}
+          <small>{props.abr}</small>
+        </h1>
       </div>
       <div className="row">
         <div className="col-md-6">
@@ -86,16 +92,16 @@ function WelcomePage(props) {
             Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. 
           </p>
           <p>
-            <a href="#docs" role="button" className="btn btn-secondary">View</a>
+            <a href="#docs" role="button" className="btn btn-secondary">Open Library</a>
           </p>
         </div>
         <div className="col-md-6">
-          <h2>Application Admin</h2>
+          <h2>Admin Console</h2>
           <p>
             Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. 
           </p>
           <p>
-            <a href="#login" role="button" className="btn btn-secondary" onClick={() => props.dispatch({type:"LOGIN"})}>Login</a>
+            <a href="#login" role="button" className="btn btn-secondary" onClick={() => props.dispatch({type:"LOGIN"})}>Launch</a>
           </p>
         </div>
       </div>
