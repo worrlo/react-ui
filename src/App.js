1980a5e78e4684d17364fb345810b5b4d5b1f451
John Worrall <theworrall@gmail.com>
2018-11-13 21:06:15 -0500

    update project to use React-Hook

--- src/App.js
+++ src/App.js
@@ -1,88 +1,107 @@
-import React, { Component } from 'react';
+import React, { useEffect, useReducer } from 'react';
 
-if (!window.feather) {
-  window.feather = {
-    replace: () => console.log("feather unknown")
-  };
+const appName = "React(HK) UI"
+function App(props) {
+  const [state, dispatch] = useReducer((state, action) => {
+    switch(action.type) {
+      case 'LOGOUT': return {
+        ...state,
+        isLoggedIn: false, 
+        name: appName
+      }
+      case 'LOGIN': return {
+        ...state,
+        isLoggedIn: true,
+        name: (appName + " Admin")
+      }
+      default: return state;
+    }
+  },{isLoggedIn:false, name: appName});
+
+  useEffect(() => {
+    window.feather.replace();
+  });
+
+  return (
+    <React.Fragment>
+      <AppNav dispatch={dispatch} {...state}>
+        {state.isLoggedIn ? <AppSignOut dispatch={dispatch} /> : ""}
+      </AppNav>
+      <main role="main">
+        {state.isLoggedIn
+          ? "<AppContent />"
+          : <WelcomePage name={state.name} dispatch={dispatch} />
+        }
+      </main>
+    </React.Fragment>
+  );
 }
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoggedIn: false,
-      name: "React-UI"
-    };
-  }
 
-  chooseContainer() {
-    return (
-      "App container"
-      + (this.state.isLoggedIn ? "-fluid" : "")
-    );
-  }
+function AppNav(props) {
+  return (
+    <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+      <a href="#home" 
+        className="navbar-brand col-sm-3 col-md-2 mr-0"
+        onClick={() => props.dispatch({type: props.isLoggedIn ? "LOGOUT" : "LOGIN"})}
+        >{props.name}</a>
+      {props.children}
+    </nav>
+  );
+}
 
-  login(val) {
-    if (val !== undefined) {
-      this.setState({isLoggedIn: val});
-    }
-    else {
-      this.setState({isLoggedIn: !this.state.isLoggedIn});
-    }
-  }
+function AppSignOut(props) {
+  return (
+    <ul className="navbar-nav px-3">
+      <li className="nav-item text-nowrap">
+        <a href="#signOut" className="nav-link" onClick={() => props.dispatch({type:"LOGOUT"})}>
+            Sign Out <span data-feather="log-out"></span>
+        </a>
+      </li>
+    </ul>
+  );
+}
 
-  componentDidMount() {
-    window.feather.replace();
-  }
-  componentDidUpdate() {
-    window.feather.replace();
-  }
+function AppFooter(props) {
+  return (
+    <footer>
+      <hr/>
+      <p>
+        Powered By: <span title="Bourbon &amp; Bad Choices">React &amp; Bootstrap</span>
+      </p>
+    </footer>
+  );
+}
 
-  render() {
-    return (
-      <div className={this.chooseContainer()}>
-        <AppNav app={this} />
+function WelcomePage(props) {
+  return (
+    <div className="container">
+      <div className="jumbotron">
+        <h1 className="display-3">{props.name}</h1>
+        <h3 className="display-4">Welcome</h3>
       </div>
-    );
-  }
-}
-
-class AppNav extends Component {
-  constructor(props) {
-    super(props);
-    this.app = props.app;
-  }
-
-  getName() {
-    return (this.app.state.name + (this.app.state.isLoggedIn ? " Admin" : ""));
-  }
-
-  render() {
-    return (
-      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-        <a href="#home" className="navbar-brand col-sm-3 col-md-2 mr-0" onClick={() => this.app.login()}>{this.getName()}</a>
-        <NavSignOut app={this.app}/>
-      </nav>
-    );
-  }
-}
-
-class NavSignOut extends Component {
-  constructor(props) {
-    super(props);
-    this.app = props.app;
-  }
-
-  render() {
-    return (!this.app.state.isLoggedIn ? "" :
-      <ul className="navbar-nav px-3">
-        <li className="nav-item text-nowrap">
-          <a href="#signOut" className="nav-link" onClick={() => this.app.login(false)}>
-             Sign Out <span data-feather="log-out"></span>
-          </a>
-        </li>
-      </ul>
-    );
-  }
+      <div className="row">
+        <div className="col-md-6">
+          <h2>Documentation</h2>
+          <p>
+            Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. 
+          </p>
+          <p>
+            <a href="#docs" role="button" className="btn btn-secondary">View</a>
+          </p>
+        </div>
+        <div className="col-md-6">
+          <h2>Application Admin</h2>
+          <p>
+            Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. 
+          </p>
+          <p>
+            <a href="#login" role="button" className="btn btn-secondary" onClick={() => props.dispatch({type:"LOGIN"})}>Login</a>
+          </p>
+        </div>
+      </div>
+      <AppFooter />
+    </div>
+  );
 }
 
 export default App;
